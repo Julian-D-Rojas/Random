bdc72cd641fdccbbf7f7fe58b2711a22a8376595
Julian-D-Rojas <julian-d-rojas@hotmail.com>
2024-06-01 13:37:33 -0500

    config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://julian-d-rojas.github.io/Random',
+  base: 'https://julian-d-rojas.github.io/Random'
 })
